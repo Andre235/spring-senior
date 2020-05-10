c2c7a360b8da33f439159ff3cf76ee1f91ff31ae
zhaojingchao <2356430623@qq.com>
2020-05-10 16:07:05 +0800

    功能：spring boot集成swagger2 说明：添加在指定环境中启用swagger2接口文档功能

--- swagger2/src/main/java/com/geek/config/Swagger2Config.java
+++ swagger2/src/main/java/com/geek/config/Swagger2Config.java
@@ -1,8 +1,10 @@
 package com.geek.config;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,6 +21,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @Configuration
+// 该注解表示在dev环境和test环境下，该配置文件才有效，即可以访问swagger-ui.html页面
+//@Profile({"dev","test"})
+// 该注解表示在特定的环境下才能开启swagger接口文档功能，这里为开发环境开启swagger功能
+@ConditionalOnProperty(name = "swagger.enable",havingValue = "true")
 public class Swagger2Config {
 
     @Bean
